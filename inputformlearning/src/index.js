fa752b17dae201a87dd8e9ea3c13e38b8e8924a6
Franyó Bellarmin <f.bellarmin93@gmail.com>
2020-04-11 12:33:00 +0200

    0.1

--- inputformlearning/src/index.js
+++ inputformlearning/src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import '../src/index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Form extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="form-input">
+          <h1>Form</h1>
+          <form>
+            <label>
+              Name:
+              <input type="text" />
+            </label>
+          </form>
+        </div>          
+        <div className="form-output">
+          <h1>Output</h1>
+        </div>
+      </div>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Form />, document.getElementById("root"));

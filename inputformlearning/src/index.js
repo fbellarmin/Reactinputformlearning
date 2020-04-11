81eac8f8a25bfb234ff46386b71d255266925b60
Franyó Bellarmin <f.bellarmin93@gmail.com>
2020-04-11 13:14:25 +0200

    0.3

--- inputformlearning/src/index.js
+++ inputformlearning/src/index.js
@@ -6,16 +6,22 @@ class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Minta János"
+      name: "Minta János",
     };
     
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   
   handleChange(event) {
     this.setState({
-      name: event.target.value
+      name: event.target.value,
     });
+  }
+  
+  handleSubmit(event) {
+    event.preventDefault();
+    alert("Mentjük már is!");
   }
   
   render() {
@@ -23,11 +29,12 @@ class Form extends React.Component {
       <div className="container">
         <div className="form-input">
           <h1>Form</h1>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <label>
               Name:
               <input type="text" onChange={this.handleChange} />
             </label>
+            <input className="submit-button" type="submit" value="submit" />
           </form>
         </div>          
         <div className="form-output">
